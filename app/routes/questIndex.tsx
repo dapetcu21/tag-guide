8e4e55aa492d8d77512bc8f528919c8e85406297
Marius Petcu <marius@petcu.me>
2025-10-27 00:21:10 +0200

    Remove unreachable code

--- app/routes/questIndex.tsx
+++ app/routes/questIndex.tsx
@@ -1,13 +1,12 @@
 import { redirect } from "react-router";
-import type { Route } from "./+types/questIndex";
 import { getSaveGame } from "~/lib/saveGame";
+import type { Route } from "./+types/questIndex";
 
 export function clientLoader({ params }: Route.ClientLoaderArgs) {
   if (getSaveGame().quests[params.questId]?.isCompleted)
     throw redirect("debrief");
 
   throw redirect("brief");
-  return {};
 }
 
 export default function Quest(_: Route.ComponentProps) {
