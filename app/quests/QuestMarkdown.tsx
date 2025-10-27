41676372d83eff3cd22aea83008b6cf64520415c
Marius Petcu <marius@petcu.me>
2025-10-27 03:16:28 +0200

    Style markdown

--- app/quests/QuestMarkdown.tsx
+++ app/quests/QuestMarkdown.tsx
@@ -1,5 +1,7 @@
 import Markdown from "react-markdown";
 
+import "./QuestMarkdown.css"
+
 export const QuestMarkdown = ({ text }: { text: string; }) => (
     <div className="quest-body">
         <Markdown>{text}</Markdown>
